a1f0ce24f67fce08a92a4973380592f87e6dbd7b
Creative-tobi <adeoyeaisha533@gmail.com>
2025-09-10 17:51:42 +0100

    ticket ordering site

--- frontend/src/component/Api.js
+++ frontend/src/component/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-//   baseURL: "https://schooldashboard-jjt8.onrender.com/db",
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ticket-ordering.onrender.com/api",
+  // baseURL: "http://localhost:5000/api"
 });
 
 Api.interceptors.request.use(
